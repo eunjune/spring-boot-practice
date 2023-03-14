2c263354be2c0a8a804b3b509b15c8a9146ae9b9
eunjun <wm10418@worksmobile.com>
2023-03-14 12:27:46 +0900

    서블릿 컨테이너 초기화

---
+++ src/main/java/hello/container/MyContainerInitV2.java
@@ -0,0 +1,28 @@
+package hello.container;
+
+import jakarta.servlet.ServletContainerInitializer;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.HandlesTypes;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Set;
+
+// AppInit의 구현체를 다 찾아서 가져옴
+@HandlesTypes(AppInit.class)
+public class MyContainerInitV2 implements ServletContainerInitializer {
+
+    @Override
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+
+        for (Class<?> appInitClass : c) {
+            try {
+                // new AppInitV1Serlvet()과 같은 코드
+                AppInit appInit = (AppInit) appInitClass.getDeclaredConstructor().newInstance();
+                appInit.onStartup(ctx);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
